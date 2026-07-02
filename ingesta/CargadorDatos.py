87a61e8f1419ea65503cfda2a91f9b35498a5709
Isaac Rodriguez <isaac_024@hotmail.com>
2026-07-01 22:47:39 -0600

    code: Load Class

---
+++ ingesta/CargadorDatos.py
@@ -0,0 +1,31 @@
+import logging
+from pathlib import Path
+from io import StringIO
+
+import requests
+import pandas as pd
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+class CargadorDatos:
+    def __init__(self, url, raw_path, processed_path):
+
+        pass
+
+    def descargar(self):
+        pass
+    
+    def filtrar_mundial(self):
+        pass
+    
+    def validar(self):
+        pass    
+
+    def guardar(self):
+        pass
+    
+    def ejecutar(self):
+        pass
+
+    
